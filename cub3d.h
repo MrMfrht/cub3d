d8c8399cee30a14065c952f6105baca1ae7d05dc
Moussa Farhat <mofarhat@c1r2s1.42beirut.com>
2025-10-07 13:00:12 +0300

    s_color

--- cub3d.h
+++ cub3d.h
@@ -3,29 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdaher <fdaher@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mofarhat <mofarhat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 14:46:01 by fdaher            #+#    #+#             */
-/*   Updated: 2025/10/03 14:29:13 by fdaher           ###   ########.fr       */
+/*   Updated: 2025/10/07 12:59:53 by mofarhat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include "get_next_line.h"
 # include "./libft/libft.h"
 # include "./minilibx-linux/mlx.h"
-# include "minilibx-linux/mlx_int.h"
 # include "get_next_line.h"
+# include "minilibx-linux/mlx_int.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_cub
 {
-    char**map;
-} t_cub;
+	char	**map;
+	t_texture * texture;
+}			t_cub;
+
+typedef struct s_color
+{
+	int red;
+	int green;
+	int blue;
+} t_color
 
 typedef struct s_texture
 {
@@ -33,19 +40,18 @@ typedef struct s_texture
 	char	*so;
 	char	*we;
 	char	*ea;
-	char	*f;
-	char	*c;
-}	t_texture;
+	t_color	*floor;
+	t_color	*ceiling;
+}			t_texture;
 
 // ...........check.c
-int		check_cub(char *str);
-int		check_xpm(char *str);
-char	*ft_strndup(const char *start, size_t len);
+int			check_cub(char *str);
+int			check_xpm(char *str);
+char		*ft_strndup(const char *start, size_t len);
 
 // ...........get_input.c
-void	free_array(char **array);
-char	**array_join(char **old_array, char *str_add);
-char	**read_from_file(const char *s);
-
+void		free_array(char **array);
+char		**array_join(char **old_array, char *str_add);
+char		**read_from_file(const char *s);
 
 #endif
